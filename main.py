a5341e6ab51a1860508dd5e986949fe29351398d
ilyas <ilyasutemisov@gmail.com>
2022-09-08 15:17:44 +0200

    3

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-truth = 'Truth'
-dare = 'Dare'
-choise = input('Make your choise {} or {}'.format(truth,dare))
+truth = 'truth'
+dare = 'dare'
 i=0
 q=0
+
+while i<3:
+    choise = input('Make your choise {} or {}: '.format(truth, dare))
+    if choise == truth:
+        print ('You choosed truth')
+    if i==3:
+        print('You choosed 3 Truths in a row, choose dare')
+        continue
+    i += 1
